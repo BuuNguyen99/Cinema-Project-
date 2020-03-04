00211dbb92674ea8cce7d3c6c99616a1269fe97c
HueLinh Nguyen <huelinh.nguyen@miracles.vn>
2020-03-04 20:07:38 +0700

    design header

--- cinema-project/src/components/Header/Header.js
+++ cinema-project/src/components/Header/Header.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import './style.css'
+import './styleHeader.css'
 
 class Header extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <div>
-                            <img src="https://www.galaxycine.vn/website/images/galaxy-logo.png" alt="logo"></img>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div>
-                            <input placeholder="Tìm tên phim, diễn viên..."></input>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div>user</div>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="container">
+                <div className="row d-flex align-items-center py-3">
+                    <div className="col-4">
+                        <img className="imageLogo" src="https://www.galaxycine.vn/website/images/galaxy-logo.png" alt="logo"></img>
+                    </div>
+                    <div className="col-4">
+                        <input className="form-control w-75 mx-auto" placeholder="Tìm tên phim, diễn viên..."></input>
+                    </div>
+                    <div className="col-4 text-right text-secondary">
+                        <a className="pl-2 text-secondary text-decoration-none" href="#"><i className="fas fa-user"></i> Đăng nhập</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
