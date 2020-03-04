7a787768754e220957f6a9e316017a90233fe01f
kirito <nguyenbuu3599@gmail.com>
2020-03-04 02:21:43 -0500

    banner_movies finish

---
+++ cinema-project/src/components/Body/HomePage/Carousel/CarouselBanner.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+class CarouselBanner extends React.Component {
+  render() {
+    return (
+      <div className="mt-5 mb-5">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.galaxycine.vn/media/2020/2/28/banner-2048x682_1582877196494.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.galaxycine.vn/media/2020/2/28/n3-ss-2048x682_1582863674634.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.galaxycine.vn/media/2020/2/28/2048x682_1582860213657.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
+  }
+}
+export default CarouselBanner;
--- cinema-project/src/components/Footer/Footer.js
+++ cinema-project/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styleFooter.css";
 
-class Footer extends Component {
+class Footer extends React.Component {
   render() {
     return (
       <div className="footer-content">
