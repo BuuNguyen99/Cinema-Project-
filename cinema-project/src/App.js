034f90581a8edd885864de163cad744c15c6721b
Ngoc <ngocqh0601@gmail.com>
2020-03-04 21:01:07 +0700

    update

--- cinema-project/src/App.js
+++ cinema-project/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer/Footer';
 import HomePage from './components/Body/HomePage/HomePage';
 
--- cinema-project/src/components/Header/Header.js
+++ cinema-project/src/components/Header/Header.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
-import './styleHeader.css'
+import './Header.css';
+import { Modal, Button } from 'react-bootstrap';
 
 class Header extends Component {
+    constructor() {
+        super()
+        this.state = {
+            show: false
+        }
+    }
+
+    handleClose = () => {
+        this.setState({
+            show: false
+        })
+    }
+
+    handleShow = () => {
+        this.setState({
+            show: true
+        })
+    }
     render() {
         return (
             <div className="container">
@@ -13,9 +32,25 @@ class Header extends Component {
                         <input className="form-control w-75 mx-auto" placeholder="Tìm tên phim, diễn viên..."></input>
                     </div>
                     <div className="col-4 text-right text-secondary">
-                        <a className="pl-2 text-secondary text-decoration-none" href="#"><i className="fas fa-user"></i> Đăng nhập</a>
+                        <a className="pl-2 text-secondary text-decoration-none" href="#" onClick={this.handleShow}><i className="fas fa-user"></i> Đăng nhập</a>
                     </div>
                 </div>
+                <Modal className="modalForm" show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Đăng nhập</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Vui lòng đăng nhập trước khi mua vé để tích luỹ điểm,
+                        cơ hội nhận thêm nhiều ưu đãi từ chương trình thành viên Galaxy Cinema.
+                    </Modal.Body>
+                    <Modal.Body>
+                        
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Đăng Nhập
+                   </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
