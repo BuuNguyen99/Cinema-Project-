0166c791a549846788b951246eaa9feaa1b2a37e
HueLinh Nguyen <huelinh.nguyen@miracles.vn>
2020-03-08 14:41:34 +0700

    fix conflict

--- cinema-project/src/App.js
+++ cinema-project/src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
-import Footer from './components/Footer/Footer';
-import HomePage from './components/Body/HomePage/HomePage';
+import React from "react";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
+import Footer from "./components/Footer/Footer";
+import routes from "./routes";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import OpenVideo from "./pages/HomePage/Movies/OpenVideo";
 
-function App() {
-  return (
-    <div className="App">
-     <Header/>
-     {/* <Navigation/> */}
-     <HomePage/>
-     <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="Container">
+          <Header />
+          <Navigation />
+          {this.showContentMenus(routes)}
+        </div>
+        <Footer/>
+      </Router>
+    );
+  }
+  showContentMenus = routes => {
+    var result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component= {route.main}
+          />
+        );
+      });
+    }
+    return <Switch> {result} </Switch>;
+  };
 }
 
-export default App;
+export default App
